833f0d7348003187ec4c13524c1f79728029df15
GauravAthode <gauravathode123@gmail.com>
2026-04-28 22:57:36 +0530

    refactor: Remove unnecessary blank line in authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
-
 export const register = (data) => API.post("/auth/register", data);
 export const verifyOtp = (data) => API.post("/auth/verify-otp", data);
 export const login = (data) => API.post("/auth/login", data);
